6b8a10870c4e2c02dce0aae2c915e526a5a8502f
Sandesh Nepali <snepali@ucdavic.edu>
2024-09-16 13:47:39 +0545

    sdfaed

--- backend/urls.py
+++ backend/urls.py
@@ -16,8 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include  # include is used to reference the ride app's URLs
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('ride.urls')),  # This includes the URLs from the ride app
 ]
+
+
+# Serve static files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
